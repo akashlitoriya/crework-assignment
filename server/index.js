dadc5108999cf1781db91117b61cb53b0859e922
akashlitoriya <96015948+akashlitoriya@users.noreply.github.com>
2024-07-31 13:16:51 +0530

    Added cors on server

--- server/index.js
+++ server/index.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const cors = require('cors')
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const taskRoutes = require('./routes/taskRoutes');
 
 
 app.use(express.json());
+app.use(cors({
+    origin: '*'
+}))
 dotenv.config();
 
 const port = process.env.PORT || 3000;
 
 connectDB();
-
 app.use('/api/v1/user', userRoutes);
 app.use('/api/v1/task', taskRoutes);
 
